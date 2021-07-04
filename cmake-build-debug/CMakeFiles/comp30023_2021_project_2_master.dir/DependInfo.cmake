fae17b93c9934ab1cbf766cc4d609f64319c7d5a
Blackmesa-Canteen <shaotienlee@foxmail.com>
2021-07-04 23:55:56 +0800

    add

---
+++ cmake-build-debug/CMakeFiles/comp30023_2021_project_2_master.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/main.c" "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/cmake-build-debug/CMakeFiles/comp30023_2021_project_2_master.dir/main.c.o"
+  "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/message_handler.c" "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/cmake-build-debug/CMakeFiles/comp30023_2021_project_2_master.dir/message_handler.c.o"
+  "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/my_dns_server_behaviour.c" "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/cmake-build-debug/CMakeFiles/comp30023_2021_project_2_master.dir/my_dns_server_behaviour.c.o"
+  "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/my_response_handler.c" "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/cmake-build-debug/CMakeFiles/comp30023_2021_project_2_master.dir/my_response_handler.c.o"
+  "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/network_handler.c" "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/cmake-build-debug/CMakeFiles/comp30023_2021_project_2_master.dir/network_handler.c.o"
+  "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/phase1.c" "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/cmake-build-debug/CMakeFiles/comp30023_2021_project_2_master.dir/phase1.c.o"
+  "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/utils.c" "/Users/shaotienlee/Desktop/Study/Courses/semster-2/Computer systems/projects/project 2/comp30023-2021-project-2-master/cmake-build-debug/CMakeFiles/comp30023_2021_project_2_master.dir/utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
